6b7979cfd75d157e76d8590eb43e8e06fd9b77d8
Vazgen Karapetyan <vkarapetyan@irobot.com>
2020-07-19 01:26:49 -0700

    Created special folder for key files.

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
+const path = require('path');
+
 module.exports = {
   HOSTNAME: 'localhost',
   PORT: 4000,
   SECRET_KEY : 'secret key',
   AUTH_ALGORITHM: 'RS256',
 
-  GRID_SERVICE_PUBLIC_KEY_FILE: 'grid_service_public.key',
+  GRID_SERVICE_PUBLIC_KEY_FILE: path.join('keys', 'grid_service_public.key'),
   GRID_SERVICE_ENDPOINT: 'grid_service',
 
-  WORKSPACE_MANAGER_PUBLIC_KEY_FILE: 'workspace_manager_public.key',
+  WORKSPACE_MANAGER_PUBLIC_KEY_FILE: path.join('keys', 'workspace_manager_public.key'),
   WORKSPACE_MANAGER_ENDPOINT: 'workspace_manager'
 }
